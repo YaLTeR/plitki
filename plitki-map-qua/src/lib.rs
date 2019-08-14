9c8dda2ac4ec0e5cfbd4b332fd1f77f169f815de
Ivan Molodetskikh <yalterz@gmail.com>
2019-08-13 19:29:36 -0700

    map-qua: add basic implementation and tests

--- plitki-map-qua/src/lib.rs
+++ plitki-map-qua/src/lib.rs
@@ -1,7 +1,155 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use std::io::{Read, Write};
+
+use plitki_core::{
+    map::{Lane, Map},
+    object::Object,
+    timing::{MapTimestamp, Timestamp},
+};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Copy, Eq, PartialEq, Hash, Serialize, Deserialize)]
+pub enum GameMode {
+    Keys4,
+    Keys7,
+}
+
+impl GameMode {
+    /// Returns the lane count of the game mode.
+    #[inline]
+    pub fn lane_count(self) -> usize {
+        match self {
+            GameMode::Keys4 => 4,
+            GameMode::Keys7 => 7,
+        }
     }
+}
+
+#[derive(Debug, Clone, Copy, Eq, PartialEq, Serialize, Deserialize)]
+pub struct HitObject {
+    #[serde(rename = "StartTime")]
+    pub start_time: i32,
+    #[serde(rename = "Lane")]
+    pub lane: i32,
+    #[serde(default, rename = "EndTime")]
+    pub end_time: i32,
+}
+
+impl HitObject {
+    /// Returns `true` if the hit object is a long note.
+    #[inline]
+    pub fn is_long_note(&self) -> bool {
+        self.end_time > 0
+    }
+}
+
+impl From<HitObject> for Object {
+    #[inline]
+    fn from(hit_object: HitObject) -> Self {
+        // TODO: this shouldn't panic and should probably be TryFrom instead.
+        if hit_object.is_long_note() {
+            Object::LongNote {
+                start: MapTimestamp(Timestamp(hit_object.start_time.checked_mul(100).unwrap())),
+                end: MapTimestamp(Timestamp(hit_object.end_time.checked_mul(100).unwrap())),
+            }
+        } else {
+            Object::Regular {
+                timestamp: MapTimestamp(Timestamp(hit_object.start_time.checked_mul(100).unwrap())),
+            }
+        }
+    }
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Serialize, Deserialize)]
+pub struct Qua {
+    #[serde(rename = "Mode")]
+    pub mode: GameMode,
+    #[serde(rename = "Title")]
+    pub title: Option<String>,
+    #[serde(rename = "Artist")]
+    pub artist: Option<String>,
+    #[serde(rename = "Creator")]
+    pub creator: Option<String>,
+    #[serde(rename = "DifficultyName")]
+    pub difficulty_name: Option<String>,
+    #[serde(rename = "HitObjects")]
+    pub hit_objects: Vec<HitObject>,
+}
+
+impl Qua {
+    /// Returns the lane count of the map.
+    #[inline]
+    pub fn lane_count(&self) -> usize {
+        self.mode.lane_count()
+    }
+}
+
+impl From<Qua> for Map {
+    #[inline]
+    fn from(qua: Qua) -> Self {
+        // TODO: this shouldn't panic and should probably be TryFrom instead.
+        let mut lanes = vec![Lane::new(); qua.lane_count()];
+        for hit_object in qua.hit_objects.into_iter() {
+            assert!(hit_object.lane > 0);
+            lanes[(hit_object.lane - 1) as usize]
+                .objects
+                .push(hit_object.into());
+        }
+
+        Self {
+            song_artist: qua.artist,
+            song_title: qua.title,
+            difficulty_name: qua.difficulty_name,
+            mapper: qua.creator,
+            lanes,
+        }
+    }
+}
+
+impl From<Map> for Qua {
+    #[inline]
+    fn from(map: Map) -> Self {
+        // TODO: this shouldn't panic and should probably be TryFrom instead.
+        Self {
+            mode: match map.lanes.len() {
+                4 => GameMode::Keys4,
+                7 => GameMode::Keys7,
+                _ => panic!("Invalid lane count: {}", map.lanes.len()),
+            },
+            artist: map.song_artist,
+            title: map.song_title,
+            difficulty_name: map.difficulty_name,
+            creator: map.mapper,
+            hit_objects: map
+                .lanes
+                .into_iter()
+                .enumerate()
+                .flat_map(|(lane, Lane { objects })| {
+                    let lane = lane as i32 + 1;
+
+                    objects.into_iter().map(move |object| match object {
+                        Object::Regular { timestamp } => HitObject {
+                            start_time: (timestamp.0).0 / 100,
+                            lane,
+                            end_time: 0,
+                        },
+                        Object::LongNote { start, end } => HitObject {
+                            start_time: (start.0).0 / 100,
+                            lane,
+                            end_time: (end.0).0 / 100,
+                        },
+                    })
+                })
+                .collect(),
+        }
+    }
+}
+
+/// Deserializes a `Qua` from an IO stream of YAML.
+pub fn from_reader<R: Read>(reader: R) -> Result<Qua, serde_yaml::Error> {
+    serde_yaml::from_reader(reader)
+}
+
+/// Serializes a `Qua` as YAML into the IO stream.
+pub fn to_writer<W: Write>(writer: W, qua: &Qua) -> Result<(), serde_yaml::Error> {
+    serde_yaml::to_writer(writer, qua)
 }
---
+++ plitki-map-qua/tests/tests.rs
@@ -0,0 +1,222 @@
+use std::{cmp::Ordering, fs::File};
+
+extern crate plitki_map_qua;
+use plitki_map_qua::{from_reader, to_writer, GameMode, HitObject, Qua};
+
+use plitki_core::{
+    map::{Lane, Map},
+    object::Object,
+    timing::{MapTimestamp, Timestamp},
+};
+use pretty_assertions::assert_eq;
+use proptest::prelude::*;
+
+#[test]
+fn parse_sample() {
+    let file = File::open("tests/data/sample.qua").unwrap();
+    let qua = from_reader(file).unwrap();
+
+    let gt = Qua {
+        mode: GameMode::Keys4,
+        artist: Some("Unknown".to_owned()),
+        title: Some("Sample Map".to_owned()),
+        difficulty_name: Some("Easy".to_owned()),
+        creator: Some("YaLTeR".to_owned()),
+        hit_objects: vec![
+            HitObject {
+                start_time: 601,
+                lane: 2,
+                end_time: 0,
+            },
+            HitObject {
+                start_time: 601,
+                lane: 1,
+                end_time: 0,
+            },
+            HitObject {
+                start_time: 601,
+                lane: 4,
+                end_time: 939,
+            },
+            HitObject {
+                start_time: 601,
+                lane: 3,
+                end_time: 939,
+            },
+            HitObject {
+                start_time: 939,
+                lane: 2,
+                end_time: 1278,
+            },
+            HitObject {
+                start_time: 939,
+                lane: 1,
+                end_time: 0,
+            },
+            HitObject {
+                start_time: 1194,
+                lane: 4,
+                end_time: 1363,
+            },
+            HitObject {
+                start_time: 1278,
+                lane: 3,
+                end_time: 0,
+            },
+        ],
+    };
+
+    assert_eq!(qua, gt);
+}
+
+#[test]
+fn convert() {
+    let file = File::open("tests/data/sample.qua").unwrap();
+    let qua = from_reader(file).unwrap();
+    let map: Map = qua.into();
+
+    let gt = Map {
+        song_artist: Some("Unknown".to_owned()),
+        song_title: Some("Sample Map".to_owned()),
+        difficulty_name: Some("Easy".to_owned()),
+        mapper: Some("YaLTeR".to_owned()),
+        lanes: vec![
+            Lane {
+                objects: vec![
+                    Object::Regular {
+                        timestamp: MapTimestamp(Timestamp(60100)),
+                    },
+                    Object::Regular {
+                        timestamp: MapTimestamp(Timestamp(93900)),
+                    },
+                ],
+            },
+            Lane {
+                objects: vec![
+                    Object::Regular {
+                        timestamp: MapTimestamp(Timestamp(60100)),
+                    },
+                    Object::LongNote {
+                        start: MapTimestamp(Timestamp(93900)),
+                        end: MapTimestamp(Timestamp(127800)),
+                    },
+                ],
+            },
+            Lane {
+                objects: vec![
+                    Object::LongNote {
+                        start: MapTimestamp(Timestamp(60100)),
+                        end: MapTimestamp(Timestamp(93900)),
+                    },
+                    Object::Regular {
+                        timestamp: MapTimestamp(Timestamp(127800)),
+                    },
+                ],
+            },
+            Lane {
+                objects: vec![
+                    Object::LongNote {
+                        start: MapTimestamp(Timestamp(60100)),
+                        end: MapTimestamp(Timestamp(93900)),
+                    },
+                    Object::LongNote {
+                        start: MapTimestamp(Timestamp(119400)),
+                        end: MapTimestamp(Timestamp(136300)),
+                    },
+                ],
+            },
+        ],
+    };
+
+    assert_eq!(map, gt);
+}
+
+#[test]
+fn parse_actual_map() {
+    let file = File::open("tests/data/actual_map.qua").unwrap();
+    from_reader(file).unwrap();
+}
+
+#[test]
+fn convert_actual_map() {
+    let file = File::open("tests/data/actual_map.qua").unwrap();
+    let qua = from_reader(file).unwrap();
+    let _map: Map = qua.into();
+}
+
+fn hit_object_compare(a: &HitObject, b: &HitObject) -> Ordering {
+    a.start_time
+        .cmp(&b.start_time)
+        .then(a.lane.cmp(&b.lane))
+        .then(a.end_time.cmp(&b.end_time))
+}
+
+prop_compose! {
+    fn arbitrary_hit_object(mode: GameMode)
+                           (start_time in 0..i32::max_value() / 100, // TODO
+                            is_long_note in any::<bool>())
+                           (start_time in Just(start_time),
+                            lane in 1..=mode.lane_count(),
+                            end_time in if is_long_note {
+                                (start_time..i32::max_value() / 100).boxed()
+                            } else {
+                                Just(0).boxed()
+                            })
+                           -> HitObject {
+        HitObject {
+            start_time,
+            lane: lane as i32,
+            end_time,
+        }
+    }
+}
+
+fn arbitrary_game_mode() -> impl Strategy<Value = GameMode> {
+    prop_oneof![Just(GameMode::Keys4), Just(GameMode::Keys7)]
+}
+
+prop_compose! {
+    fn arbitrary_qua()
+                    (mode in arbitrary_game_mode())
+                    (mode in Just(mode),
+                     title in prop::option::of(any::<String>()),
+                     artist in prop::option::of(any::<String>()),
+                     creator in prop::option::of(any::<String>()),
+                     difficulty_name in prop::option::of(any::<String>()),
+                     hit_objects in prop::collection::vec(arbitrary_hit_object(mode), 0..64))
+                    -> Qua {
+        Qua {
+            mode,
+            title,
+            artist,
+            creator,
+            difficulty_name,
+            hit_objects,
+        }
+    }
+}
+
+proptest! {
+    #[test]
+    fn qua_to_map_and_back(mut qua in arbitrary_qua()) {
+        let map: Map = qua.clone().into();
+        let mut qua2: Qua = map.into();
+
+        qua.hit_objects.sort_unstable_by(hit_object_compare);
+        qua2.hit_objects.sort_unstable_by(hit_object_compare);
+
+        prop_assert_eq!(qua, qua2);
+    }
+
+    #[test]
+    fn qua_serialize_deserialize(mut qua in arbitrary_qua()) {
+        let mut buf = Vec::new();
+        to_writer(&mut buf, &qua).unwrap();
+        let mut qua2 = from_reader(&buf[..]).unwrap();
+
+        qua.hit_objects.sort_unstable_by(hit_object_compare);
+        qua2.hit_objects.sort_unstable_by(hit_object_compare);
+
+        prop_assert_eq!(qua, qua2);
+    }
+}
