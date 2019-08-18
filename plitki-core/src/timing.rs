c938ec33077858786ce1b0d6e27f65b460819659
Ivan Molodetskikh <yalterz@gmail.com>
2019-08-18 10:57:37 -0700

    core: derive +,-,+=,-= on timestamps

--- plitki-core/src/timing.rs
+++ plitki-core/src/timing.rs
@@ -1,15 +1,22 @@
 //! Types and utilities related to timing.
+use derive_more::{Add, AddAssign, Sub, SubAssign};
 
 /// A point in time.
 ///
 /// Timestamps are represented as `i32`s in <sup>1</sup>⁄<sub>100</sub>ths of a millisecond.
-#[derive(Debug, Clone, Copy, Hash, Eq, PartialEq, Ord, PartialOrd)]
+#[derive(
+    Debug, Clone, Copy, Hash, Eq, PartialEq, Ord, PartialOrd, Add, AddAssign, Sub, SubAssign,
+)]
 pub struct Timestamp(pub i32);
 
 /// A point in time, measured in map time.
-#[derive(Debug, Clone, Copy, Hash, Eq, PartialEq, Ord, PartialOrd)]
+#[derive(
+    Debug, Clone, Copy, Hash, Eq, PartialEq, Ord, PartialOrd, Add, AddAssign, Sub, SubAssign,
+)]
 pub struct MapTimestamp(pub Timestamp);
 
 /// A point in time, measured in game time.
-#[derive(Debug, Clone, Copy, Hash, Eq, PartialEq, Ord, PartialOrd)]
+#[derive(
+    Debug, Clone, Copy, Hash, Eq, PartialEq, Ord, PartialOrd, Add, AddAssign, Sub, SubAssign,
+)]
 pub struct GameTimestamp(pub Timestamp);
