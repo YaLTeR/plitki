1905662a01df5f580adff5b797798cf15571f8b5
Ivan Molodetskikh <yalterz@gmail.com>
2019-08-31 13:20:44 -0700

    ui-wayland: extract code into SingleFrameRenderer

--- plitki-ui-wayland/src/renderer.rs
+++ plitki-ui-wayland/src/renderer.rs
@@ -13,7 +13,7 @@ use palette::{ComponentWise, Srgba};
 use plitki_core::{
     object::Object,
     state::{LongNoteState, ObjectState},
-    timing::GameTimestamp,
+    timing::{GameTimestamp, MapTimestamp},
 };
 use slog_scope::{debug, trace};
 
@@ -45,6 +45,19 @@ pub struct Renderer {
     sprites: Vec<Sprite>,
 }
 
+pub struct SingleFrameRenderer<'a> {
+    renderer: &'a mut Renderer,
+    state: &'a GameState,
+    elapsed_timestamp: GameTimestamp,
+    lane_width: f32,
+    border_offset: f32,
+    border_width: f32,
+    judgement_line_position: f32,
+    note_height: f32,
+    first_visible_timestamp: MapTimestamp,
+    one_past_last_visible_timestamp: MapTimestamp,
+}
+
 struct Sprite {
     pos: Point2<f32>,
     scale: Vector2<f32>,
@@ -67,6 +80,14 @@ fn compute_ortho(dimensions: (u32, u32)) -> Ortho<f32> {
         near: -1.,
         far: 1.,
     }
+}
+
+fn to_scroll_speed_coord(x: f32) -> f32 {
+    x * 5.
+}
+
+fn from_scroll_speed_coord(x: f32) -> f32 {
+    x / 5.
 }
 
 impl Renderer {
@@ -163,118 +184,12 @@ impl Renderer {
             self.projection = self.ortho.into();
         }
 
-        self.sprites.clear();
-
-        let lane_width = 0.2;
-        let lane_count = state.map.lanes.len();
-        let border_offset = lane_width * lane_count as f32 / 2.;
-        let border_width = 0.01;
-        let judgement_line_position = self.ortho.bottom + 0.4;
-
-        // Left lane border.
-        self.sprites.push(Sprite {
-            pos: Point2::new(-border_offset - border_width, self.ortho.bottom),
-            scale: Vector2::new(border_width, self.ortho.top - self.ortho.bottom),
-            color: Srgba::new(1., 1., 1., 1.),
-        });
-        // Right lane border.
-        self.sprites.push(Sprite {
-            pos: Point2::new(border_offset, self.ortho.bottom),
-            scale: Vector2::new(border_width, self.ortho.top - self.ortho.bottom),
-            color: Srgba::new(1., 1., 1., 1.),
-        });
-
-        let elapsed_timestamp = GameTimestamp(elapsed.try_into().unwrap());
-
-        #[allow(clippy::inconsistent_digit_grouping)]
-        let to_scroll_speed_coord = |x| x * 5.;
-        #[allow(clippy::inconsistent_digit_grouping)]
-        let from_scroll_speed_coord = |x| x / 5.;
-
-        let note_height = 0.1;
-        let first_visible_timestamp = (elapsed_timestamp
-            - to_scroll_speed_coord(judgement_line_position - self.ortho.bottom + note_height)
-                / state.scroll_speed)
-            .to_map(state);
-        let one_past_last_visible_timestamp = (elapsed_timestamp
-            + to_scroll_speed_coord(self.ortho.top - judgement_line_position) / state.scroll_speed)
-            .to_map(state);
-
-        for (lane, objects, object_states) in (0..state.map.lanes.len()).map(|lane| {
-            (
-                lane,
-                &state.map.lanes[lane].objects[..],
-                &state.lane_states[lane].object_states[..],
-            )
-        }) {
-            let first_visible_index = objects
-                .binary_search_by_key(&first_visible_timestamp, Object::end_timestamp)
-                .unwrap_or_else(identity);
-            let one_past_last_visible_index = objects
-                .binary_search_by_key(&one_past_last_visible_timestamp, Object::start_timestamp)
-                .unwrap_or_else(identity);
-
-            let range = first_visible_index..one_past_last_visible_index;
-            for (object, object_state) in objects[range.clone()]
-                .iter()
-                .zip(object_states[range].iter())
-                .rev()
-                .filter(|(_, s)| !s.is_hit())
-            {
-                let start = match *object {
-                    Object::Regular { .. } => object.start_timestamp(),
-                    Object::LongNote { start, end } => match *object_state {
-                        ObjectState::LongNote(LongNoteState::Held) => {
-                            state.game_to_map(elapsed_timestamp).min(end).max(start)
-                        }
-
-                        ObjectState::LongNote(LongNoteState::Missed {
-                            held_until: Some(held_until),
-                        }) => held_until.max(start),
-
-                        _ => start,
-                    },
-                };
-
-                let pos = Point2::new(
-                    -border_offset + lane_width * lane as f32,
-                    judgement_line_position
-                        + from_scroll_speed_coord(
-                            (state.map_to_game(start) - elapsed_timestamp) * state.scroll_speed,
-                        ),
-                );
-
-                let height = match *object {
-                    Object::Regular { .. } => note_height,
-                    Object::LongNote { end, .. } => {
-                        from_scroll_speed_coord((end - start).to_game(state) * state.scroll_speed)
-                    }
-                };
-
-                let mut color = if lane == 0 || lane == 3 {
-                    Srgba::new(0.5, 0.5, 0.5, 0.5)
-                } else {
-                    Srgba::new(0.00, 0.25, 0.5, 0.5)
-                };
-
-                if let ObjectState::LongNote(LongNoteState::Missed { .. }) = *object_state {
-                    color = color.component_wise_self(|x| x * 0.1);
-                }
-
-                self.sprites.push(Sprite {
-                    pos,
-                    scale: Vector2::new(lane_width, height),
-                    color,
-                });
-            }
+        {
+            let mut renderer = SingleFrameRenderer::new(self, elapsed, state);
+            renderer.push_borders();
+            renderer.push_objects();
+            renderer.push_judgement_line();
         }
-
-        // Judgement line.
-        self.sprites.push(Sprite {
-            pos: Point2::new(-border_offset, judgement_line_position),
-            scale: Vector2::new(border_offset * 2., border_width),
-            color: Srgba::new(1., 1., 1., 1.),
-        });
 
         let instance_data = self.build_instance_data();
 
@@ -306,5 +221,160 @@ impl Renderer {
         frame.finish().unwrap();
 
         trace!("finished redraw"; "time_taken" => ?(Instant::now() - start));
+    }
+}
+
+impl<'a> SingleFrameRenderer<'a> {
+    fn new(renderer: &'a mut Renderer, elapsed: Duration, state: &'a GameState) -> Self {
+        let elapsed_timestamp = GameTimestamp(elapsed.try_into().unwrap());
+
+        let lane_width = 0.2;
+        let lane_count = state.map.lanes.len();
+        let border_offset = lane_width * lane_count as f32 / 2.;
+        let border_width = 0.01;
+        let judgement_line_position = renderer.ortho.bottom + 0.4;
+        let note_height = 0.1;
+
+        let first_visible_timestamp = (elapsed_timestamp
+            - to_scroll_speed_coord(judgement_line_position - renderer.ortho.bottom + note_height)
+                / state.scroll_speed)
+            .to_map(state);
+        let one_past_last_visible_timestamp = (elapsed_timestamp
+            + to_scroll_speed_coord(renderer.ortho.top - judgement_line_position)
+                / state.scroll_speed)
+            .to_map(state);
+
+        renderer.sprites.clear();
+
+        Self {
+            renderer,
+            state,
+            elapsed_timestamp,
+            lane_width,
+            border_offset,
+            border_width,
+            judgement_line_position,
+            note_height,
+            first_visible_timestamp,
+            one_past_last_visible_timestamp,
+        }
+    }
+
+    fn push_borders(&mut self) {
+        // Left lane border.
+        self.renderer.sprites.push(Sprite {
+            pos: Point2::new(
+                -self.border_offset - self.border_width,
+                self.renderer.ortho.bottom,
+            ),
+            scale: Vector2::new(
+                self.border_width,
+                self.renderer.ortho.top - self.renderer.ortho.bottom,
+            ),
+            color: Srgba::new(1., 1., 1., 1.),
+        });
+        // Right lane border.
+        self.renderer.sprites.push(Sprite {
+            pos: Point2::new(self.border_offset, self.renderer.ortho.bottom),
+            scale: Vector2::new(
+                self.border_width,
+                self.renderer.ortho.top - self.renderer.ortho.bottom,
+            ),
+            color: Srgba::new(1., 1., 1., 1.),
+        });
+    }
+
+    fn push_objects(&mut self) {
+        // Yay, partial borrowing to win vs. the borrow checker...
+        let state = self.state;
+
+        for (lane, objects, object_states) in (0..self.state.map.lanes.len()).map(|lane| {
+            (
+                lane,
+                &state.map.lanes[lane].objects[..],
+                &state.lane_states[lane].object_states[..],
+            )
+        }) {
+            let first_visible_index = objects
+                .binary_search_by_key(&self.first_visible_timestamp, Object::end_timestamp)
+                .unwrap_or_else(identity);
+            let one_past_last_visible_index = objects
+                .binary_search_by_key(
+                    &self.one_past_last_visible_timestamp,
+                    Object::start_timestamp,
+                )
+                .unwrap_or_else(identity);
+
+            let range = first_visible_index..one_past_last_visible_index;
+            for (object, object_state) in objects[range.clone()]
+                .iter()
+                .zip(object_states[range].iter())
+                .rev()
+                .filter(|(_, s)| !s.is_hit())
+            {
+                self.renderer
+                    .sprites
+                    .push(self.object_sprite(lane, object, object_state));
+            }
+        }
+    }
+
+    fn object_sprite(&self, lane: usize, object: &Object, object_state: &ObjectState) -> Sprite {
+        let start = match *object {
+            Object::Regular { .. } => object.start_timestamp(),
+            Object::LongNote { start, end } => match *object_state {
+                ObjectState::LongNote(LongNoteState::Held) => self
+                    .elapsed_timestamp
+                    .to_map(self.state)
+                    .min(end)
+                    .max(start),
+
+                ObjectState::LongNote(LongNoteState::Missed {
+                    held_until: Some(held_until),
+                }) => held_until.max(start),
+
+                _ => start,
+            },
+        };
+
+        let pos = Point2::new(
+            -self.border_offset + self.lane_width * lane as f32,
+            self.judgement_line_position
+                + from_scroll_speed_coord(
+                    (self.state.map_to_game(start) - self.elapsed_timestamp)
+                        * self.state.scroll_speed,
+                ),
+        );
+
+        let height = match *object {
+            Object::Regular { .. } => self.note_height,
+            Object::LongNote { end, .. } => {
+                from_scroll_speed_coord((end - start).to_game(self.state) * self.state.scroll_speed)
+            }
+        };
+
+        let mut color = if lane == 0 || lane == 3 {
+            Srgba::new(0.5, 0.5, 0.5, 0.5)
+        } else {
+            Srgba::new(0.00, 0.25, 0.5, 0.5)
+        };
+
+        if let ObjectState::LongNote(LongNoteState::Missed { .. }) = *object_state {
+            color = color.component_wise_self(|x| x * 0.1);
+        }
+
+        Sprite {
+            pos,
+            scale: Vector2::new(self.lane_width, height),
+            color,
+        }
+    }
+
+    fn push_judgement_line(&mut self) {
+        self.renderer.sprites.push(Sprite {
+            pos: Point2::new(-self.border_offset, self.judgement_line_position),
+            scale: Vector2::new(self.border_offset * 2., self.border_width),
+            color: Srgba::new(1., 1., 1., 1.),
+        });
     }
 }
